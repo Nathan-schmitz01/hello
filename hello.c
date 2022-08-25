e7f32dee95fea15ef58175ad6a441e17b7fa390a
Nathan Schmitz <nathanschmitz@unomaha.edu>
2022-08-25 19:36:40 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Nathan Schmitz
+ *         nathanschmitz@unomaha.edu
+ * Date: 2022/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Nathan Schmitz?\n");
+  printf("Major: exploratory studies\n");
+
+  return 0;
+}
